52ef4aa2a8758d9e7cb3e22658d1e35d5054d49c
cacauzinha-fofinha <00001114463966sp@al.educacao.sp.gov.br>
2024-11-21 11:51:27 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -16,8 +16,4 @@ criaCartao(
     'Uma função é um bloco de código que executa alguma tarefa'
 )
 
-criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
-)
+
